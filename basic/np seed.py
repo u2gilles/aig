670ef6f828e41c232cdd6ce2b330e40e94f2c55a
u2gilles <u2gilles@gmail.com>
2018-04-12 09:50:29 +0700

    first commit

---
+++ basic/np seed.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+""" Set Random Seeds for same results """
+
+np.random.seed(101)
+rand_a = np.random.uniform(0, 100, (2, 2))
+print ('\n',rand_a)
+
+np.random.seed(101)
+rand_a = np.random.uniform(0, 100, (2, 2))
+print ('\n',rand_a)
+
+rand_a = np.random.uniform(0, 100, (2, 2))
+print ('\n',rand_a)
